f0098c6e53f9cf10d9b0e2503b0a07d8a49caa3b
wilson <154498819+wilson7yz@users.noreply.github.com>
2023-12-21 15:36:49 +0800

    Update fuckDinner.js: Modify the effective domain name
    【修改生效的域名】忘记改域名了

--- fuckDinner.js
+++ fuckDinner.js
@@ -4,7 +4,7 @@
 // @version      2023-12-21
 // @description  try to take over the world!
 // @author       You
-// @match        *://*/*
+// @match        *://github.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @run-at       document-idle
